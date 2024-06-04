34036e47c981d767639066ca1cc2784342c8b5bb
Peter Kapakos <peterk@provokesolutions.com>
2024-06-04 15:32:08 -0700

    tenant

--- api/src/providers/JwtProvider.ts
+++ api/src/providers/JwtProvider.ts
@@ -15,7 +15,7 @@ interface Key {
 }
 
 export class JwtProvider {
-    public static readonly KEY_ENDPOINT = `https://login.microsoftonline.com/${process.env.TENANT_ID}/discovery/keys`;
+    public static readonly KEY_ENDPOINT = `https://login.microsoftonline.com/80133990-1076-49d7-b8ee-1d5098569f89/discovery/keys`;
     // public static readonly KEY_ENDPOINT = 'https://login.microsoftonline.com/common/discovery/keys';
     public static readonly AUDIENCE = `api://${"1f83ba3b-1066-4946-935d-e9ac61cb3f21"}`;
 

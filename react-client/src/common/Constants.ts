c33a35d0241462585bd1a3a2c57995dde8b33575
Intikhab Alam <intikhab.alam@nintex.com>
2024-06-26 20:54:32 +1200

    renaming variables

--- react-client/src/common/Constants.ts
+++ react-client/src/common/Constants.ts
@@ -7,5 +7,5 @@ export const AUTH_AUTHORITY = `https://login.microsoftonline.com/${process.env.R
 export const SP_ROOT_SITE_URL = process.env.REACT_APP_SP_ROOT_SITE_URL;
 export const REACT_APP_TENANT_ID = process.env.REACT_APP_TENANT_ID;
 export const AZURE_CLIENT_SECRET = process.env.REACT_APP_AZURE_APP_CLIENT_SPECIAL;
-export const AZURE_CLIENT_CERT_THUMBPRINT = process.env.REACT_APP_AZURE_CLIENT_CERT_THUMBPRINT;
-export const AZURE_CLIENT_CERT_PRIVATE_KEY = process.env.REACT_APP_AZURE_CLIENT_CERT_PRIVATE_KEY;
+export const AZURE_CLIENT_CERT_THUMBPRINT = process.env.AZURE_CLIENT_CERT_THUMBPRINT;
+export const AZURE_CLIENT_CERT_PRIVATE_KEY = process.env.AZURE_CLIENT_CERT_PRIVATE_KEY;

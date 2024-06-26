61a554f053043a9c1e7e10cbae8d10f6b3d2daf1
Intikhab Alam <intikhab.alam@nintex.com>
2024-06-26 19:51:23 +1200

    removing tenant id contstant value13

--- api/src/providers/AuthProvider.ts
+++ api/src/providers/AuthProvider.ts
@@ -20,8 +20,8 @@ export abstract class AuthProvider {
                 privateKey: process.env.AZURE_CLIENT_CERT_PRIVATE_KEY
             };
         } else {
-            // auth.clientSecret = process.env.AZURE_CLIENT_SECRET;
-             auth.clientSecret = "OXH8Q~AKCfVBMUZw~bxYpoE1GIN.iAYUJLDsLciM";
+             auth.clientSecret = process.env.AZURE_CLIENT_SECRET;
+            // auth.clientSecret = "OXH8Q~AKCfVBMUZw~bxYpoE1GIN.iAYUJLDsLciM";
         }
         this.client = new ConfidentialClientApplication({ auth: auth });
     }
--- react-client/src/common/Constants.ts
+++ react-client/src/common/Constants.ts
@@ -6,6 +6,6 @@ export const SPE_CONTAINER_TYPE_ID = process.env.REACT_APP_SPE_CONTAINER_TYPE_ID
 export const AUTH_AUTHORITY = `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`;
 export const SP_ROOT_SITE_URL = process.env.REACT_APP_SP_ROOT_SITE_URL;
 export const REACT_APP_TENANT_ID = process.env.REACT_APP_TENANT_ID;
-export const AZURE_CLIENT_SECRET = process.env.REACT_APP_AZURE_CLIENT_SECRET;
+export const AZURE_CLIENT_SECRET = process.env.REACT_APP_AZURE_APP_CLIENT_SPECIAL;
 export const AZURE_CLIENT_CERT_THUMBPRINT = process.env.REACT_APP_AZURE_CLIENT_CERT_THUMBPRINT;
 export const AZURE_CLIENT_CERT_PRIVATE_KEY = process.env.REACT_APP_AZURE_CLIENT_CERT_PRIVATE_KEY;

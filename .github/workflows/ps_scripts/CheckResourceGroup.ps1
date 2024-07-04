8f74027ccf3c2f36389c1455718d2ab06a2d70a7
Intikhab Alam <intikhab.alam@nintex.com>
2024-07-05 08:26:09 +1200

    adding ps script to check resourcegroup

---
+++ .github/workflows/ps_scripts/CheckResourceGroup.ps1
@@ -0,0 +1,15 @@
+param (
+    [string]$resourceGroupName,
+    [string]$location
+)
+
+# Check if the resource group exists
+$resourceGroup = az group show --name $resourceGroupName -o json 2>&1
+
+if ($resourceGroup -notmatch "ResourceNotFound") {
+    Write-Output "Resource Group $resourceGroupName already exists"
+    Write-Output "::set-output name=rg_exists::true"
+} else {
+    az group create --name $resourceGroupName --location $location
+    Write-Output "Resource Group created."
+}

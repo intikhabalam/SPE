bfdcd0f5e564b146fffaff9886f3196ed7524fc8
Intikhab Alam <intikhab.alam@nintex.com>
2024-07-09 09:51:58 +1200

    adding alternative version for az cli module-6

--- .github/ps_scripts/CheckResourceGroup.ps1
+++ .github/ps_scripts/CheckResourceGroup.ps1
@@ -3,14 +3,20 @@ param (
     [string]$location
 )
 
-# Check if the resource group exists
-$resourceGroup = az group show --name $resourceGroupName -o json 2>&1
-az group show --name $resourceGroupName -o json
-echo $resourceGroup
-if ($resourceGroup -notmatch "ResourceNotFound") {
+try {
+    # Check if the resource group exists
+    $resourceGroup = az group show --name $resourceGroupName -o json
     Write-Output "Resource Group $resourceGroupName already exists"
-    Write-Output "::set-output name=rg_exists::true"
-} else {
-    az group create --name $resourceGroupName --location $location
-    Write-Output "Resource Group created."
+    echo "rg_exists=true" >> $env:GITHUB_ENV
+} catch {
+    # If the resource group does not exist, create it
+    if ($_ -match "ResourceGroupNotFound") {
+        az group create --name $resourceGroupName --location $location
+        Write-Output "Resource Group $resourceGroupName created."
+        echo "rg_exists=false" >> $env:GITHUB_ENV
+    } else {
+        # If there's another error, output the error message and fail the script
+        Write-Error "An error occurred: $_"
+        exit 1
+    }
 }
